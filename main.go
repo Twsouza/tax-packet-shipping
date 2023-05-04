dca2b1134c2ab63d27e45b3a411a7999d79a4298
Taynan <Twsouza@users.noreply.github.com>
2023-05-04 11:03:21 -0300

    Remove graceful stop
    It's just a test

--- main.go
+++ main.go
@@ -40,27 +40,7 @@ func main() {
 	}
 
 	log.Println("App is now listening...")
-	// Run our server in a goroutine so that it doesn't block.
-	go func() {
-		if err := srv.ListenAndServe(); err != nil {
-			log.Println("err on server", err)
-		}
-	}()
-
-	c := make(chan os.Signal, 1)
-	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
-	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
-	signal.Notify(c, os.Interrupt)
-
-	// Block until we receive our signal.
-	<-c
-
-	// Create a deadline to wait for.
-	ctx, cancel := context.WithTimeout(context.Background(), wait)
-	defer cancel()
-	// Doesn't block if no connections, but will otherwise wait
-	// until the timeout deadline.
-	srv.Shutdown(ctx)
-	log.Println("shutting down")
-	os.Exit(0)
+	if err := srv.ListenAndServe(); err != nil {
+		log.Println("err on server", err)
+	}
 }
